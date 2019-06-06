e8d9ac2e4213273d33333fbb709f5abcc35f5e03
admls <adam.the.sherwood@gmail.com>
2019-06-06 23:35:02 +0100

    init navbar

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,37 @@
+<script>
+  // import Button from "../components/Button.svelte";
+  import {Link} from 'svero'
+</script>
+
+<style>
+  nav {
+    width: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    padding: 20px;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  button {
+    box-sizing: border-box;
+    margin: auto;
+    position: relative;
+    outline: none;
+    border: none;
+    padding: 5px;
+    background: none;
+  }
+
+  button:hover {
+    outline: solid 1px rgb(159, 135, 124);
+  }
+</style>
+
+<nav>
+  <Link href="/"><button>Home</button></Link>
+  <Link href="/bio"><button>Bio</button></Link>
+  <Link href="/projects"><button>Projects</button></Link>
+  <Link href="/cv"><button>CV</button></Link>
+</nav>
