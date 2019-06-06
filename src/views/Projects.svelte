e972cd97f73f3402b120921b909a3b95d8d71a83
admls <adam.the.sherwood@gmail.com>
2019-06-07 00:58:52 +0100

    add flexbox iterates over projects

--- src/views/Projects.svelte
+++ src/views/Projects.svelte
@@ -1,1 +1,22 @@
+<script>
+  import Project from '../components/Project.svelte'
+  import projects from '../data/projects'
+  
+</script>
+
 <h3>Projects</h3>
+<p>Hello hello hello</p>
+<div>
+{#each projects as project}
+  <Project {project} />
+{/each}
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
