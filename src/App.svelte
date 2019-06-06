3a5ab4e1b93ee98567b40db194b0fdaaa383088f
admls <adam.the.sherwood@gmail.com>
2019-06-06 23:34:12 +0100

    add routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
-	export let name;
+  export let name;
+  import Navbar from './layout/Navbar.svelte'
+  import Home from './views/Home.svelte'
+  import CV from './views/CV.svelte'
+  import Bio from './views/Bio.svelte'
+  import Projects from './views/Projects.svelte'
+  import {Router, Route} from 'svero'
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<Navbar />
+<Router>
+  <Route path="*" component={Home} />
+  <Route path="/projects" component={Projects} />
+  <Route path="/bio" component={Bio} />
+  <Route path="/cv" component={CV} />
+</Router>
 
-<h1>Hello {name}!</h1>
