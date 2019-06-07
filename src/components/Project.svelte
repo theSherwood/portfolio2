4294667c9b242b153e2458753a79b0d8fff6809e
admls <adam.the.sherwood@gmail.com>
2019-06-07 01:00:45 +0100

    init project card component

---
+++ src/components/Project.svelte
@@ -0,0 +1,21 @@
+<script>
+  import GlobeIcon from "./GlobeIcon.svelte";
+  import GithubIcon from "./GithubIcon.svelte";
+
+  export let project;
+</script>
+
+<div>
+<h3>{project.title}</h3>
+<p>{project.description}</p>
+<GlobeIcon />
+<GithubIcon />
+</div>
+
+<style>
+  div {
+    padding: 20px;
+    border: solid 1px rgba(85, 85, 85, 0.5);
+    flex: 0 0 300px;
+  }
+</style>
