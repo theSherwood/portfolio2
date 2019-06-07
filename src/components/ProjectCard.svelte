4cb1864dcd77736100d40b63edb8e6dfc4ab3462
admls <adam.the.sherwood@gmail.com>
2019-06-07 01:33:43 +0100

    add markup and some style

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -3,13 +3,31 @@
   import GithubIcon from "./GithubIcon.svelte";
 
   export let project;
+
+  const {title, description, image, website, github, moreInfo} = project;
 </script>
 
-<div>
-<h3>{project.title}</h3>
-<p>{project.description}</p>
-<GlobeIcon />
-<GithubIcon />
+<div class="container">
+  <h3>{title}</h3>
+  <p>{description}</p>
+  <section class="row">
+    <a href={website} target="_blank" rel="noopener noreferrer" class="six columns">
+      <button>
+        <span>
+          <span>Website</span>
+          <GlobeIcon />
+        </span>
+      </button>
+    </a>
+    <a href={github} target="_blank" rel="noopener noreferrer" class="six columns">
+      <button>
+        <span>
+          <span>Source</span>
+          <GithubIcon />
+        </span>
+      </button>
+    </a>
+  </section>
 </div>
 
 <style>
@@ -17,5 +35,10 @@
     padding: 20px;
     border: solid 1px rgba(85, 85, 85, 0.5);
     flex: 0 0 300px;
+    margin: 20px;
+  }
+
+  button {
+    width: 100%;
   }
 </style>
