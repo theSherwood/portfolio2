38259a78e3ecfeb6cb77ef95cc357f96df70e08d
admls <adam.the.sherwood@gmail.com>
2019-06-07 01:02:00 +0100

    init projects data structure

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    title: "RegexXP",
+    description: "Something",
+    image: "none",
+    website: "some link",
+    github: "some link",
+    moreInfo: "another link"
+  },
+  {
+    title: "BiblioStack",
+    description: "Something",
+    image: "none",
+    website: "some link",
+    github: "some link",
+    moreInfo: "another link"
+  },
+  {
+    title: "Mentat",
+    description: "Something",
+    image: "none",
+    website: "some link",
+    github: "some link",
+    moreInfo: "another link"
+  },
+  {
+    title: "ReconJS",
+    description: "Something",
+    image: "none",
+    website: "some link",
+    github: "some link",
+    moreInfo: "another link"
+  }
+];
+
+export default projects;
