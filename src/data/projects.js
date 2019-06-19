5c03f6b55c2e29507675734a3115fdfe6054e841
admls <adam.the.sherwood@gmail.com>
2019-06-19 10:36:25 +0100

    change link structure and add links

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,33 +3,51 @@ const projects = [
     title: "RegexXP",
     description: "Something",
     image: "none",
-    website: "https://www.regexxp.online",
-    github: "some link",
-    moreInfo: "another link"
+    links: {
+      website: "https://www.regexxp.online",
+      github: "https://github.com/theSherwood/RegexXP",
+      info: "another link"
+    }
   },
   {
-    title: "BiblioStack",
+    title: "Bibliostack",
     description: "Something",
     image: "none",
-    website: "some link",
-    github: "some link",
-    moreInfo: "another link"
+    links: {
+      website: "https://www.bibliostack.com",
+      github: "https://github.com/theSherwood/Bibliostack",
+      info: "another link"
+    }
   },
   {
     title: "Mentat",
     description: "Something",
     image: "none",
-    website: "some link",
-    github: "some link",
-    moreInfo: "another link"
+    links: {
+      website: "some link",
+      github: "some link",
+      info: "another link"
+    }
   },
   {
     title: "ReconJS",
     description: "Something",
     image: "none",
-    website: "some link",
-    github: "some link",
-    moreInfo: "another link"
+    links: {
+      website: "",
+      github: "https://github.com/theSherwood/ReconJS",
+      info: "another link"
+    }
+  },
+  {
+    title: "ReconJS Site",
+    description: "Something",
+    image: "none",
+    links: {
+      website: "http://reconjs-site.thesherwood.now.sh/",
+      github: "https://github.com/theSherwood/reconjs-site",
+      info: "another link"
+    }
   }
 ];
 
