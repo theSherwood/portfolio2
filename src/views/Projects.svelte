ec8c9080d02c54e730621f03f74b9d6e17845c4f
admls <adam.the.sherwood@gmail.com>
2019-06-20 15:14:02 +0100

    add grid formatting

--- src/views/Projects.svelte
+++ src/views/Projects.svelte
@@ -1,22 +1,31 @@
 <script>
-  import ProjectCard from '../components/ProjectCard.svelte'
-  import projects from '../data/projects'
+  import ProjectCard from "../components/ProjectCard.svelte"
+  import projects from "../data/projects"
   import { route } from "../stores/route";
   export let router = {};
   route.set(router.route.path);
 </script>
 
-<div>
-{#each projects as project}
-  <ProjectCard {project} />
-{/each}
+<div class="grid">
+  {#each projects as project}
+    <ProjectCard {project} />
+  {/each}
 </div>
 
 <style>
-  div {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
+  .grid {
+    width: 90%;
+    max-width: 800px;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
   }
+
+  @media only screen and (max-width: 400px) {
+    .grid {
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
+  }
+
 </style>
