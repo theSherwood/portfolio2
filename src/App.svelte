05d882c085f77e549b6e6226a8b6350921ae26b9
admls <adam.the.sherwood@gmail.com>
2019-06-20 21:18:54 +0100

    add routes to projectPages

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,12 @@
   import Bio from './views/Bio.svelte'
   import Projects from './views/Projects.svelte'
   import {Router, Route} from 'svero'
+
+  import Regexxp from './views/projectPages/Regexxp.svx'
+  import Bibliostack from './views/projectPages/Bibliostack.svx'
+  import Mentat from './views/projectPages/Mentat.svx'
+  import Reconjs from './views/projectPages/Reconjs.svx'
+  import ReconjsSite from './views/projectPages/ReconjsSite.svx'
 </script>
 
 <Navbar />
@@ -14,5 +20,12 @@
   <Route path="/projects" component={Projects} />
   <Route path="/bio" component={Bio} />
   <Route path="/cv" component={CV} />
+
+  <!-- Project Routes -->
+  <Route path="/regexxp" component={Regexxp} />
+  <Route path="/bibliostack" component={Bibliostack} />
+  <Route path="/mentat" component={Mentat} />
+  <Route path="/reconjs" component={Reconjs} />
+  <Route path="/reconjs-site" component={ReconjsSite} />
 </Router>
 
