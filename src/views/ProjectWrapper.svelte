929830f28404d0b59f52ff9d321daea2b8c57311
admls <adam.the.sherwood@gmail.com>
2019-06-22 22:35:09 +0100

    add darker underlay for readability

--- src/views/ProjectWrapper.svelte
+++ src/views/ProjectWrapper.svelte
@@ -37,5 +37,18 @@
     max-width: 800px;
     margin: auto;
     padding: 8px;
+    position: relative;
+  }
+
+  main::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgb(0, 0, 0);
+    opacity: 0.7;
+    z-index: -1;
   }
 </style>
