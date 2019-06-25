e8c45ded1de6778b24b7af322f451ff939bde849
admls <adam.the.sherwood@gmail.com>
2019-06-25 13:07:48 +0100

    init delayLink

---
+++ src/components/DelayLink.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from "svelte";
+  import { navigateTo } from "svero";
+
+  let cssClass = "";
+  export let href = "/";
+  export let className = "";
+  export { cssClass as class };
+  export let delay = 0;
+
+  onMount(() => {
+    className = className || cssClass;
+  });
+
+  function handleClick() {
+    if (delay) {
+      setTimeout(() => {
+        navigateTo(href);
+      }, delay);
+    } else {
+      navigateTo(href);
+    }
+  }
+</script>
+
+<a href="{href}" class="{className}" on:click|preventDefault="{handleClick}">
+  <slot></slot>
+</a>
