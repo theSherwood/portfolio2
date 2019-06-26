14ab9236490d8ecf286a159ba0a66526128b5572
admls <adam.the.sherwood@gmail.com>
2019-06-26 09:33:07 +0100

    add name

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -3,3 +3,25 @@
   export let router = {};
   route.set(router.route.path);
 </script>
+
+<div class="title-container">
+  <h1 class="title">Adam</h1>
+  <h1 class="title">Sherwood</h1>
+</div>
+
+<style>
+  .title {
+    font-family: futura-pt, Helvetica, Arial, sans-serif;
+    letter-spacing: 0.6em;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: rgba(255, 255, 255, 0.3);
+    margin: 0;
+    line-height: 0.8em;
+  }
+
+  .title-container {
+    position: absolute;
+    top: 50vh;
+  }
+</style>
