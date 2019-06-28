840f50e4ad69548770ac6e117a4054cdddbcc00c
admls <adam.the.sherwood@gmail.com>
2019-06-28 01:02:35 +0100

    register service-worker

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ("serviceWorker" in navigator) {
+  console.log("CLIENT: service worker registration in progress.");
+  navigator.serviceWorker.register("../service-worker.js").then(
+    function() {
+      console.log("CLIENT: service worker registration complete.");
+    },
+    function() {
+      console.log("CLIENT: service worker registration failure.");
+    }
+  );
+} else {
+  console.log("CLIENT: service worker is not supported.");
+}
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  intro: true,
+  props: {
+    name: "world"
+  }
 });
 
 export default app;
