7749d8232fe43d59d272b27aa11b988193c299ed
admls <adam.the.sherwood@gmail.com>
2019-07-02 14:28:00 +0100

    init contactLinks component

---
+++ src/components/ContactLinks.svelte
@@ -0,0 +1,54 @@
+<script>
+  import EmailIcon from './EmailIcon.svelte';
+  import TwitterIcon from './TwitterIcon.svelte';
+  import GithubIcon from './GithubIcon.svelte';
+
+  // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+  // Random encryption key feature coded by Andrew Moulden
+  // This code is freeware provided these four comment lines remain intact
+  // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+  let coded = "xlxB.E1C.w1CHZttl@TBxFk.ftB"
+  let key = "oWC9ID6T43UJZkBpfGOhsd8QxbNtL7in2rSjqeMyvXgVYR1wlmzcA5EPuFKaH0"
+  let shift=coded.length
+  let link=""
+  for (let i=0; i<coded.length; i++) {
+    let ltr;
+    if (key.indexOf(coded.charAt(i))==-1) {
+      ltr = coded.charAt(i)
+      link += (ltr)
+    }
+    else {     
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+      link += (key.charAt(ltr))
+    }
+  }
+</script>
+
+<a href="{'mailto:'+link}'">
+  <EmailIcon size="1.5em"/>
+  <span>{link}</span>
+</a>
+<a href="https://github.com/theSherwood" target="_blank" rel="noopener noreferrer">
+  <GithubIcon size="1.5em"/>
+  <span>@theSherwood</span>
+</a>
+<a href="https://twitter.com/adamthesherwood" target="_blank" rel="noopener noreferrer">
+  <TwitterIcon size="1.5em"/>
+  <span>@adamthesherwood</span>
+</a>
+
+<style>
+  a {
+    display: block;
+    width: max-content;
+  }
+
+  :global(svg) {
+    vertical-align: bottom;
+  }
+
+  span {
+    margin-left: .5em;
+  }
+
+</style>
