7d8195b769ec143e53e3395a88e739a52f04ee1d
admls <adam.the.sherwood@gmail.com>
2019-07-02 18:20:17 +0100

    init ReplLinks

---
+++ src/components/ReplLinks.svelte
@@ -0,0 +1,17 @@
+<script>
+  import ELink from "../views/projectPages/links/ELink.svelte";
+</script>
+
+<ELink href="https://repl.it/languages">REPL.it</ELink>
+<ELink href="https://www.tutorialspoint.com/execute_lisp_online.php"
+  >Common Lisp</ELink
+>
+<ELink href="http://tryarc.org/">Arc Lisp</ELink>
+<ELink href="https://www.tutorialspoint.com/compile_assembly_online.php"
+  >Assembly</ELink
+>
+<ELink href="https://svelte.dev/repl/hello-world">Svelte3</ELink>
+<ELink href="https://ellie-app.com/new">Elm</ELink>
+<ELink href="https://astexplorer.net/">JavaScript AST</ELink>
+<ELink href="https://www.graphqlbin.com/v2/new">GraphQL</ELink>
+<ELink href="https://developer.github.com/v4/explorer/">GitHub GraphQL</ELink>
