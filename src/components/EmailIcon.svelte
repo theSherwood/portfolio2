6f21d844440740df7a17aff8d4aba1d79a6c02ba
admls <adam.the.sherwood@gmail.com>
2019-07-02 14:27:49 +0100

    init email and twitter icons

---
+++ src/components/EmailIcon.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let size = "1em";
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  x="0px"
+  y="0px"
+  width="{size}"
+  height="{size}"
+  viewBox="0 0 172 172"
+  style=" fill:currentColor;"
+>
+  <g
+    fill="none"
+    fill-rule="nonzero"
+    stroke="none"
+    stroke-width="1"
+    stroke-linecap="butt"
+    stroke-linejoin="miter"
+    stroke-miterlimit="10"
+    stroke-dasharray=""
+    stroke-dashoffset="0"
+    font-family="none"
+    font-weight="none"
+    font-size="none"
+    text-anchor="none"
+    style="mix-blend-mode: normal"
+  >
+    <path d="M0,172v-172h172v172z" fill="none"></path>
+    <g fill="currentColor">
+      <g id="surface1">
+        <path
+          d="M0,24.08v123.84h172v-123.84zM158.24,55.98063l-72.24,40.9575l-72.24,-40.9575v-8.11625l72.24,40.9575l72.24,-40.9575z"
+        ></path>
+      </g>
+    </g>
+  </g>
+</svg>
